595aea30c35770d7121d1458d5242f1bf33acfc4
shekhargrace14 <82882207+shekhargrace14@users.noreply.github.com>
2023-12-18 12:22:43 +0000

    Hello

--- script.js
+++ script.js
@@ -5,8 +5,21 @@ let faqAns = document.getElementById("faqAns")
 fetch("faq.json")
   .then(response=> response.json())
   .then(data=>{ 
-    console.log(data); 
-    for(let i in data){
-        console.log(i)
-    }
+    // console.log(data.que); 
+    // for(let i in data){
+    //     console.log(i)
+    // }
+    data.map((value)=>{
+      // console.log(value.que)
+      // console.log(value.ans)
+
+
+
+      faqQue.innerHTML = value.que
+      faqAns.innerHTML = value.ans
+
+
+    })
+
+
 })      
