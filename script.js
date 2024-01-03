333b4244ced568ac8b1c53be8b86a25185afaed5
shekhargrace14 <82882207+shekhargrace14@users.noreply.github.com>
2024-01-03 09:11:35 +0000

    faq Changes

--- script.js
+++ script.js
@@ -1,25 +1,10 @@
-let faqQue = document.getElementById("faqQue") 
-let faqAns = document.getElementById("faqAns") 
+var icons = document.getElementsByClassName("icons");
+var top = document.getElementsByClassName("top");
+var bottom = document.getElementsByClassName("bottom");
+var faqrow = document.getElementById("faqrow");
 
 
-fetch("faq.json")
-  .then(response=> response.json())
-  .then(data=>{ 
-    // console.log(data.que); 
-    // for(let i in data){
-    //     console.log(i)
-    // }
-    data.map((value)=>{
-      // console.log(value.que)
-      // console.log(value.ans)
+top.addEventListener("click",function(){
+  top.classList.toggle("hello");
 
-
-
-      faqQue.innerHTML = value.que
-      faqAns.innerHTML = value.ans
-
-
-    })
-
-
-})      
+})
