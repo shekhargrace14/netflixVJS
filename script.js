7109b3d8c04aa96a93a27e214ec510f4395e5848
shekhargrace14 <82882207+shekhargrace14@users.noreply.github.com>
2024-01-05 17:15:22 +0000

    faq.json fetching

--- script.js
+++ script.js
@@ -2,20 +2,60 @@
 let faqrow = document.querySelectorAll(".faqrow")
 let que =  document.querySelectorAll(".que")
 let icon = document.querySelectorAll("ion-icon")
-// let ans = document.querySelectorAll(".ans")
+let ans = document.querySelectorAll(".ans")
 
 
-// flag = 0;
-
-faqrow.forEach((faq, i)=>{
 
 
-  faq.addEventListener("click", function(){
-    console.log("hello")
-    let ans = faq.firstElementChild.nextElementSibling;
-    console.log(ans);
-    icon[i].classList.toggle("iconRotate");
 
-    ans.classList.toggle("active")
+
+
+fetch('faq.json')
+.then((response)=>{return response.json()})
+.then((data)=>{console.log(data)
+  // return data;
+  
+  let dd=""
+  data.map((value)=>{
+    // console.log(value.ques)
+    // console.log(value.ans)
+
+    dd+=` 
+            <div class="column que d-flex justify-between align-center">
+                <h3>${value.ques}</h3>
+                <div class="div">
+                    <ion-icon name="add-outline"></ion-icon>
+                </div>
+            </div>
+            <div class="column ans ">
+                <p>${value.ans}</p>
+            </div>
+            </div>`
+
+
   })
+  document.getElementById("faq").innerHTML=dd;
+
+  faqrow.forEach((faq, i)=>{
+  
+    faq.addEventListener("click", function(){
+      
+      icon[i].classList.toggle("iconRotate")
+
+      for(let i=0; i < faqrow.length; i++){
+        if(faqrow[i] !=faq){
+          ans[i].classList.remove("active")
+        }else{
+          ans[i].classList.toggle("active")
+        }
+      }
+    })
+  })
+
 })
+
+
+
+// faq.json api call ends
+
+
