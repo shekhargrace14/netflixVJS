66f3c7ceefcca30b8e3c605a94d766251bffcc56
shekhargrace14 <82882207+shekhargrace14@users.noreply.github.com>
2024-01-09 07:08:00 +0000

    faq

--- script.js
+++ script.js
@@ -5,91 +5,50 @@ let icon = document.querySelectorAll("ion-icon")
 let ans = document.querySelectorAll(".ans")
 
 
-async function fetchData(){
-  const response = await fetch('faq.json')
-  const data = await response.json();
+// async function fetchData(){
+//   const response = await fetch('faq.json')
+//   const data = await response.json();
 
-  console.log(data)
+//   console.log(data)
 
-  return data;
+//   return data;
 
-}
+// }
 
-fetchData();
+// fetchData();
 
-async function renderData(){ 
-  const data = await fetchData();
-  data.forEach((item)=>{
-    icon.innerHTML = item.ques
-    // console.log(item.ques)
-    // console.log(item.ans)
+// async function renderData(){ 
+//   const data = await fetchData();
+//   data.forEach((item)=>{
+//     icon.innerHTML = item.ques
+//     // console.log(item.ques)
+//     // console.log(item.ans)
 
-  })
-  let card = "";
-  data.map((value)=>{
-    card+=`<div class="row faqrow" id="faq">
-          <div class="column que d-flex justify-between align-center">
-              <h3>${value.ques}</h3>
-              <div class="div">
-                  <ion-icon name="add-outline"></ion-icon>
-              </div>
-          </div>
-          <div class="column ans ">
-              <p>${value.ans}</p>
-          </div>
-      </div>`
-  })
-  document.querySelector('.faqrow').innerHTML = card;
+//   })
+//   let card = "";
+//   data.map((value)=>{
+//     card+=`<div class="row faqrow" id="faq">
+//           <div class="column que d-flex justify-between align-center">
+//               <h3>${value.ques}</h3>
+//               <div class="div">
+//                   <ion-icon name="add-outline"></ion-icon>
+//               </div>
+//           </div>
+//           <div class="column ans ">
+//               <p>${value.ans}</p>
+//           </div>
+//       </div>`
+//   })
+//   document.querySelector('.faqrow').innerHTML = card;
 
-faqrow.forEach((faq, i)=>{
-  
-  faq.addEventListener("click", function(){
-    let icon = document.querySelectorAll("ion-icon")
-    
-    icon.classList.toggle("iconRotate")
 
-    for(let i=0; i < faqrow.length; i++){
-      if(faqrow[i] !=faq){
-        ans[i].classList.remove("active")
-      }else{
-        ans[i].classList.toggle("active")
-      }
-    }
-  })
-})
-}
-
-renderData()
-
-// fetch('faq.json')
-// .then((response)=>{return response.json()})
-// .then((data)=>{console.log(data)
-  // return data;
-  
-  // let dd=""
-  // data.map((value)=>{
-    // console.log(value.ques)
-    // console.log(value.ans)
-  //   dd+=` 
-  //           <div class="column que d-flex justify-between align-center">
-  //               <h3>${value.ques}</h3>
-  //               <div class="div">
-  //                   <ion-icon name="add-outline"></ion-icon>
-  //               </div>
-  //           </div>
-  //           <div class="column ans ">
-  //               <p>${value.ans}</p>
-  //           </div>
-  //           </div>`
-  // })
-  // document.getElementById("faq").innerHTML=dd;
-// })
 
 // faqrow.forEach((faq, i)=>{
   
 //   faq.addEventListener("click", function(){
+//     let icon = document.querySelectorAll("ion-icon")
     
-//     icon[i].classList.toggle("iconRotate")
+//     icon.classList.toggle("iconRotate")
 
 //     for(let i=0; i < faqrow.length; i++){
 //       if(faqrow[i] !=faq){
@@ -102,6 +61,38 @@ renderData()
 // })
 
 
+// }
+
+// renderData()
+
+
 // faq.json api call ends
+
+
+
+faqrow.forEach((item, i)=>{
+  
+  item.addEventListener("click",function(){
+    // icon[i-1].classList.toggle("iconRotate")
+  
+    
+    // ans[i-1].classList.toggle("active");
+
+    for(let j=0; j< faqrow.length; j++){
+      if(faqrow[j+1] !=item){
+        ans[j].classList.remove("active");
+        icon[j].classList.remove("iconRotate")
+
+      }else{
+        ans[j].classList.toggle("active")
+        icon[j].classList.toggle("iconRotate")
+
+      }
+    }
+
+  console.log(icon)
+    
+  })
+})
 
 
