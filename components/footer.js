60a5bd64ca9d701b6dd09447c5a16767c1dd53d2
shekhargrace14 <82882207+shekhargrace14@users.noreply.github.com>
2024-01-16 08:24:45 +0000

    footer changes

---
+++ components/footer.js
@@ -0,0 +1,5 @@
+fetch("./components/footer.html")
+.then((response)=>response.text())
+.then((data)=>
+    document.querySelector("footer").innerHTML = data
+)
--- home.js
+++ home.js
@@ -3,29 +3,46 @@
 // const URL = "https://api.themoviedb.org/3/movie/";
 // const API = "185c7d1fa7ff15b5023522fae491e666";
 
-async function apiData(query){
+async function apiData(query="love"){
     const resolve = await fetch(`https://api.themoviedb.org/3/search/movie?query=${query}&api_key=185c7d1fa7ff15b5023522fae491e666`);
     const data = await  resolve.json();
     console.log(data)
     return data; 
 }
-apiData("race");
-
-
-    
-// })
-
+apiData();
 
 let input = document.querySelector("#input");
 let submitInput = document.querySelector("#submit");
 
-
 submitInput.addEventListener('click', function(){
     const value = input.value
     apiData(value);
+    console.log("hello", value)
      
 })
 
+async function rowdata() {
+    const data = await apiData();
+    console.log(data,"rowdata")
+    let card = "";
+    data.map((value)=>{
+    
+    card+=`<div class="row">
+                <div class="column">
+                    <div class="poster">
+                        <img src=${value.poster_path} alt="">
+                    </div>
+                </div>
+                <div class="column">
+
+                </div>
+            </div>
+        `
+
+    })
+    document.querySelector('.latestRow').innerHTML = card;
+}
+rowdata()
 
 
 
