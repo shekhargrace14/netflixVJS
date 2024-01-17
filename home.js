5fdbfac47fe32fe8b1bcb1c92c73d7593803762e
shekhargrace14 <82882207+shekhargrace14@users.noreply.github.com>
2024-01-17 15:42:51 +0530

    pc code

--- home.js
+++ home.js
@@ -3,46 +3,83 @@
 // const URL = "https://api.themoviedb.org/3/movie/";
 // const API = "185c7d1fa7ff15b5023522fae491e666";
 
-async function apiData(query="love"){
-    const resolve = await fetch(`https://api.themoviedb.org/3/search/movie?query=${query}&api_key=185c7d1fa7ff15b5023522fae491e666`);
-    const data = await  resolve.json();
-    console.log(data)
-    return data; 
-}
-apiData();
+
+
+
+
+// async function apiData(query="money"){
+//     const resolve = await fetch(`https://api.themoviedb.org/3/search/movie?query=${query}&api_key=185c7d1fa7ff15b5023522fae491e666`);
+//     const data = await  resolve.json();
+//     console.log(data)
+//     return data; 
+// }
+// apiData();
+// var newValue = '';
+
+// submitInput.addEventListener('click', function(){
+//     var value = input.value   
+//     newValue = value;
+//     console.log("hello", value)   
+// })
+
+// console.log("newValue", newValue)
+
+
+
+
+
+// async function rowdata() {
+//     const data = await apiData();
+//     // console.log(data)
+
+  
+//     const latestRow = document.querySelector(".latestRow")
+
+//     data.results.map((value)=>{
+//     latestRow.insertAdjacentHTML(
+//         "beforebegin",
+//         `<div class="row">
+//         <div class="column">
+//         <div class="poster">
+//             <h3>${value.title}</h3>
+//         <img src=${value.poster_path} alt="">
+//         </div>
+//         </div>
+//         </div>`
+//         )
+//     })
+
+
+// }
+// rowdata()
 
 let input = document.querySelector("#input");
 let submitInput = document.querySelector("#submit");
 
 submitInput.addEventListener('click', function(){
-    const value = input.value
-    apiData(value);
-    console.log("hello", value)
-     
+    var value = input.value;   
+    fetch(`https://api.themoviedb.org/3/search/movie?query=${value}&api_key=185c7d1fa7ff15b5023522fae491e666`)
+    .then((resolve)=>resolve.json())
+    .then((data)=>{
+    console.log(data)
+    
+        const latestRow = document.querySelector(".latestRow");
+        data.results.map((value)=>{
+            latestRow.insertAdjacentHTML("beforebegin",`
+                <div class="row">
+                <div class="column">
+                <div class="poster">
+                <h3>${value.title}</h3>
+                <img src=${value.poster_path} alt="">
+                </div>
+                </div>
+                </div>
+            `)
+        })
+    })
 })
 
-async function rowdata() {
-    const data = await apiData();
-    console.log(data,"rowdata")
-    let card = "";
-    data.map((value)=>{
-    
-    card+=`<div class="row">
-                <div class="column">
-                    <div class="poster">
-                        <img src=${value.poster_path} alt="">
-                    </div>
-                </div>
-                <div class="column">
 
-                </div>
-            </div>
-        `
-
-    })
-    document.querySelector('.latestRow').innerHTML = card;
-}
-rowdata()
 
 
 
