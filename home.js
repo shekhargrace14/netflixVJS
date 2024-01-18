cc746f6dbba0dadfae86d75a24bdc38433f8746a
shekhargrace14 <82882207+shekhargrace14@users.noreply.github.com>
2024-01-18 12:38:11 +0000

    home hero created

--- home.js
+++ home.js
@@ -1,6 +1,6 @@
 // 185c7d1fa7ff15b5023522fae491e666
 
-// const URL = "https://api.themoviedb.org/3/movie/";
+// const URL = "https://api.themoviedb.org/3/search/movie";
 // const API = "185c7d1fa7ff15b5023522fae491e666";
 
 
@@ -53,33 +53,66 @@
 // }
 // rowdata()
 
-let input = document.querySelector("#input");
-let submitInput = document.querySelector("#submit");
+// let input = document.querySelector("#input");
+// let submitInput = document.querySelector("#submit");
 
-submitInput.addEventListener('click', function(){
-    var value = input.value;   
-    fetch(`https://api.themoviedb.org/3/search/movie?query=${value}&api_key=185c7d1fa7ff15b5023522fae491e666`)
-    .then((resolve)=>resolve.json())
-    .then((data)=>{
-    console.log(data)
+// submitInput.addEventListener('click', function(){
+//     var value = input.value;   
+//     console.log(value)
+
+
+//     fetch(`${URL}?query=${value}&api_key=${API}`)
+//     .then((resolve)=>resolve.json())
+//     .then((data)=>{
+//     console.log(data)
     
-        const latestRow = document.querySelector(".latestRow");
-        data.results.map((value)=>{
-            latestRow.insertAdjacentHTML("beforebegin",`
-                <div class="row">
-                <div class="column">
-                <div class="poster">
-                <h3>${value.title}</h3>
-                <img src=${value.poster_path} alt="">
+//         const latestRow = document.querySelector(".latestRow");
+//         data.results.map((value)=>{
+//             latestRow.insertAdjacentHTML("beforebegin",`
+//                 <div class="row">
+//                 <div class="column">
+//                 <div class="poster">
+//                 <h3>${value.title}</h3>
+//                 <img src=${value.poster_path} alt="">
+//                 </div>
+//                 </div>
+//                 </div>
+//             `)
+//         })
+//     })
+//     value = " ";
+//     console.log(value)
+// })
+
+
+
+
+
+fetch("https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1&api_key=185c7d1fa7ff15b5023522fae491e666")
+.then((response)=>response.json()).then((data)=> {
+    console.log(data)
+    const heroCarousel = document.querySelector(".heroCarousel");
+    data.results.map((value)=>{        
+        heroCarousel.insertAdjacentHTML("beforeend",`
+        <div class="row">
+            <div class="innerRow">
+                <div class="column" >
+                    <h4>${value.original_title}</h4>
+                    <p>${value.overview}</p>
                 </div>
+                <div class="column " >
+                    <h1>Hello</h1>
                 </div>
+            </div>
+            <div class="innerRow homeHeroBG">
+                <div class="column ">
+                    <img src="https://image.tmdb.org/t/p/w500${value.backdrop_path}"   alt="">
                 </div>
-            `)
-        })
+            </div>
+        </div>
+        
+    `)
     })
+    console.log(heroCarousel)
+
 })
-
-
-
-
-
