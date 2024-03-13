511f2d103aa30407da5041a3f175d91896695565
shekhargrace14 <shekhargrace14@gmail.com>
2024-03-13 22:53:27 +0530

    removefromMylist updates

--- mylist.js
+++ mylist.js
@@ -15,39 +15,59 @@ fetch("https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1&api_
         return myListItemsArray.includes(element.id.toString())
 
     })
-    let myListCarousel = document.querySelector(".myListCarousel")
-    console.log(myListCarousel, "myListCarousel");
+    let cardRow = document.querySelector(".cardRow")
+    // console.log(cardRow, "cardRow");
     myListMatchedItems.map((items)=>{
-        myListCarousel.insertAdjacentHTML("beforeend",`
+        cardRow.insertAdjacentHTML("beforeend",`
         
         <div class="column cardMain">
-        <a href="./singleMoviePage.html">    
-            <div class="card">
-        
-            <img src="https://image.tmdb.org/t/p/w500${items.backdrop_path}" alt="${items.id}">
-            <div class="details">
-                <h4>${items.title}</h4>
-                <div class="d-flex justify-between">
-                    <i class="fa-solid fa-circle-play"></i>
-                    <i class="fa-solid fa-thumbs-up"></i>
-                    <i class="fa-solid fa-thumbs-down"></i>
-        
-                    <i class="fa-solid fa-plus" onclick="addToMyList()"></i>
-                    <i class="fa-solid fa-chevron-down"></i>
-                </div>
-                <ul class="d-flex justify-between">
-                    <li>Action</li>
-                    <li>Drama</li>
-                    <li>Adventure</li>
-                </ul>
-            </div>
-            </div>
-        </a>
-    </div>
-        `)
+            
+                <div class="card d-flex">
+                    <div class="image">
 
-        
+                        <img src="https://image.tmdb.org/t/p/w500${items.poster_path}" alt="${items.id}">
+                    </div>
+
+                    <div class="details">
+                        <h3>${items.title}</h3>
+                        <p class="overview">${items.overview}</p>
+                        <p class="release_date">${items.release_date}</p>
+                        <p class="vote_average">${items.vote_average}</p>
+                        <p class="origin_country">${items.origin_country}</p>
+                        <p class="original_language">${items.original_language}</p>
+                        <p class="vote_average">${items.vote_average}</p>
+                        <p class="vote_count">${items.vote_count}</p>
+                        <button class="removeFromMyListBtn" id=${items.id}>Remove from My List</button>
+                    </div>
+                </div>
+         
+        </div>`)
+       
     })
     
+    let removeFromMyListBtn =  document.querySelectorAll(".removeFromMyListBtn")
+    // console.log(removeFromMyListBtn, "removeFromMyListBtn")
+    removeFromMyListBtn.forEach((item)=>{
+        item.addEventListener("click",()=>{
+            console.log(item.id, "item")
+            let myListBeforeDeleting = JSON.parse(localStorage.getItem("myListItemsArray"));
+            console.log(myListBeforeDeleting, "myListBeforeDeleting")
+            let itemToRemoveFromMyList = item.id
+            console.log(itemToRemoveFromMyList, "itemToRemoveFromMyList")
+
+            let myListAfterDeletingItem = []
+            myListAfterDeletingItem = myListBeforeDeleting.filter(item=>item !== itemToRemoveFromMyList)
+            console.log(myListAfterDeletingItem, "myListAfterDeletingItem")
+
+            localStorage.setItem("myListItemsArray", JSON.stringify(myListAfterDeletingItem))
+
+            let cardRow = document.querySelector("cardRow")
+            cardRow.remove()
+
+            // myListItemsArray = 
+            // myListItemsArray.reomve(item.id)
+            // console.log(localStorage.setItem("myListItemsArray", JSON.stringify(myListAfterDeletingItem)))
+        })
+    })
 
 })
